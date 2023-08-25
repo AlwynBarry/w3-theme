097a8bd2afc0917cd90f57b2c91f93fc6b4ed269
Alwyn Barry <alwyn_barry@yahoo.co.uk>
2023-08-25 15:56:15 +0100

    Version Bump single.php
    Version Bump

--- single.php
+++ single.php
@@ -8,7 +8,7 @@
  * @copyright	2021
  * For use in	w3-theme
  * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * @version		1.0.0
+ * @version		1.0.2
  * 
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
  * General Public License as published by the Free Software Foundation; either version 2 of the License, 
